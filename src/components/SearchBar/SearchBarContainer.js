f112c5da2d563d45c73326a844df059325d87305
christianlewis024 <58643162+christianlewis024@users.noreply.github.com>
2020-03-17 16:08:44 -0700

    finished, maybe stretch? probably not though

--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -8,11 +8,9 @@ const SearchBar = () => {
       <div className="image-wrapper">
         <i className="fab fa-instagram" />
       </div>
+      <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"></img>
       <form className="search-form">
-        <input
-          type="text"
-          placeholder="Search"
-        />
+        <input type="text" placeholder="Search" />
       </form>
       <div className="social-wrapper">
         <div className="social">
